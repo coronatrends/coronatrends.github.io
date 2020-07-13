b08c97a71d1a3009ee942b63956f9dd0486483c2
G <gerardbriscoe@gmail.com>
2020-07-13 19:14:20 +0100

    'gui update: dropdown ordering update'

--- vue-definitions.js
+++ vue-definitions.js
@@ -2,7 +2,7 @@
 // custom graph component
 
 var point = {
-'Austria':172,'Belgium':172,'Canada':172,'Czechia':172,'Estonia':172,'France':172,'Germany':170,'Ireland':172,'Japan':170,'Latvia':172,'Lima':172,'Lithuania':172,'London':172,'Mexico':167,'Netherlands':172,'New Zealand':170,'Panama':89,'Peru':172,'Poland':172,'Slovenia':172,'South Africa':172,'South Korea':172,'Spain':121,'Sweden':172,'Switzerland':172,
+'Austria':172,'Belgium':172,'Canada':172,'Czechia':172,'Estonia':172,'Germany':170,'Ireland':172,'Japan':170,'Latvia':172,'Lima':172,'Lithuania':172,'London':172,'Mexico':167,'Netherlands':172,'New Zealand':170,'Panama':89,'Peru':172,'Poland':172,'Slovenia':172,'South Africa':172,'South Korea':172,'Spain':121,'Sweden':172,'Switzerland':172,
 'default':173
 };
 
@@ -582,7 +582,7 @@ window.app = new Vue({
 		'-------------',
 		'North America','South America','Latin America','Europe','EU','Middle East','Africa','Asia','Oceania',
 		'-------------',
-		'USA (3441275)','Brazil (1867841)','India (906380)','Russia (733699)','Peru (326326)','Chile (317657)','Mexico (299750)','UK (290133)','South Africa (276242)','Italy (243230)','Germany (200323)','France (170752)','Colombia (150445)','Canada (107807)','Argentina (100166)','China (83602)','Belgium (62707)','Netherlands (51093)','Poland (38190)','Switzerland (32946)','Ireland (25628)','Austria (18948)','Czechia (13204)','Australia (11644)','Estonia (2014)','Slovenia (1849)','New Zealand (1544)',
+		'USA (3442728)','Brazil (1867841)','India (906617)','Russia (733699)','Peru (326326)','Chile (317657)','Mexico (299750)','UK (290133)','South Africa (276242)','Italy (243230)','Germany (200323)','France (170752)','Colombia (150445)','Canada (107807)','Argentina (100166)','China (83602)','Belgium (62707)','Netherlands (51093)','Poland (38190)','Switzerland (32946)','Ireland (25638)','Austria (18948)','Czechia (13204)','Australia (11644)','Estonia (2014)','Slovenia (1849)','New Zealand (1544)',
 		'-------------',
 		'England','Scotland','Wales',
 		'-------------',
